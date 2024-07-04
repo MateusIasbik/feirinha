0450eff3d8a790f24f2d0009dced36714d510b35
Mateus <mateusmpi@gmail.com>
2024-07-04 13:01:57 -0300

    fix: Get with all items or filtered

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import httpStatus from "http-status";
 
 const app = express();
 app.use(cors());
@@ -8,47 +9,65 @@ app.use(express.json());
 let shoppingList = [
     {
         id: 1,
-        name: "laranja",
+        name: "Laranja",
         quantity: 10,
         type: "fruta"
     },
     {
         id: 2,
-        name: "melancia",
+        name: "Melancia",
         quantity: 5,
         type: "fruta"
     },
     {
         id: 3,
-        name: "banana",
+        name: "Banana",
         quantity: 12,
         type: "fruta"
     },
     {
         id: 4,
-        name: "batata",
+        name: "Batata",
         quantity: 8,
         type: "legume"
     },
     {
         id: 5,
-        name: "abóbora",
+        name: "Abóbora",
         quantity: 4,
         type: "legume"
     },
     {
         id: 6,
-        name: "leite",
+        name: "Leite",
         quantity: 10,
         type: "lácteo"
     },
     {
         id: 7,
-        name: "queijo",
+        name: "Queijo",
         quantity: 4,
         type: "lácteo"
     },
 ];
+
+// MUDAR O 5001 PARA 5000 ANTES DE ENTREGAR O PROJETO
+
+app.get("/items", (req, res) => {
+
+    const { type, name } = req.query;
+    let filterItems = shoppingList;
+
+    if (type) {
+        filterItems = filterItems.filter(item => item.type === type);
+    } 
+    if (name) {
+        filterItems = filterItems.filter(item => item.name === name)
+    } 
+        res.status(httpStatus.OK).send(filterItems);
+})
+
+
 
 app.post("/items", (req, res) => {
 
@@ -56,12 +75,12 @@ app.post("/items", (req, res) => {
     const {name, quantity, type} = req.body;
 
     if(!name || !quantity || !type) {
-        res.status(422).send("Todos os campos são obrigatórios!");
+        res.status(httpStatus.UNPROCESSABLE_ENTITY).send("Todos os campos são obrigatórios!");
         return;
     }
 
     if(shoppingList.find(name => shoppingList.name === name)) {
-        res.status(409).send("Este produto já existe!");
+        res.status(httpStatus.CONFLICT).send("Este produto já existe!");
         return;
     }
 
@@ -70,12 +89,10 @@ app.post("/items", (req, res) => {
         ...item
     });
 
-    res.status(201).send("Item adicionado com sucesso!");
+    res.status(httpStatus.CREATED).send("Item adicionado com sucesso!");
 })
 
-
-
-
-app.listen(5000, () => {
+// MUDAR O 5001 PARA 5000 ANTES DE ENTREGAR O PROJETO
+app.listen(5001, () => {
     console.log("Rodando em http://localhost:5000");
   });
