168a6e0edbee8729f53066295e724ceffa1e3284
Mateus <mateusmpi@gmail.com>
2024-07-04 10:43:34 -0300

    Initial backend project of a shopping list Rest API

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+
+
+
+server.listen(5000, () => {
+    console.log("Rodando em http://localhost:5000");
+  });
